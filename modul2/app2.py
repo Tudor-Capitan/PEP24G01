4f08186b71839f2266c3e9ceadf17069be85e374
Tudor-Capitan <capitantudor@yahoo.com>
2024-01-29 20:42:01 +0200

    Test file

---
+++ modul2/app2.py
@@ -0,0 +1,17 @@
+#1
+inp = input('Input height:')
+print(inp)
+print(type(inp))
+
+prn = print('out')
+print(prn)
+print(type(prn))
+
+
+
+#2
+h = input('Input height:')
+b = input('Input base:')
+h = int(h)
+b = int(b)
+print(h + b)
---
+++ modul2/part1.py
@@ -0,0 +1,18 @@
+str1 = 'hello world'
+print(type(str1))
+int1 = 100
+print(type(int1))
+none = None
+print(type(none))
+
+result = str1.capitalize()
+print('Returned type:', type(result))
+
+str1 = 'hello world {} {}'
+result = str1.format('test' , 'test2')
+print('Formated String:', result)
+
+str1 = 'hello world'
+result = str1.center(20, '#')
+print('Centered String:', result)
+
